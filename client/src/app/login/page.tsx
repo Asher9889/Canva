35f367b6b270860af40a478e7824f81af9767d9d
Asher9889 <saursbhkushwaha9889@gmail.com>
2025-06-16 15:28:16 +0530

    adding google auth

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,7 +1,31 @@
+import LoginCard from "@/components/login/LoginCard";
+import Image from "next/image";
+
 const Login = ()=>{
     return (
-        <div>
-            Login Page
+        <div className="min-h-screen relative">
+            <div className="absolute inset-0 bg-cover"
+            style={{
+               backgroundImage:"url(https://static.canva.com/web/images/002d737e30b90787006afee36ee53149.jpg)"
+            }}
+            />
+        
+            <div className="absolute inset-0 opacity-85 bg-gradient-to-b from-black via-transparent  to-black" />
+
+            <div>
+                <Image
+                src="https://static.canva.com/web/images/856bac30504ecac8dbd38dbee61de1f1.svg"
+                alt="canva"
+                className="absolute top-5 left-8"
+                width={80}
+                height={80}
+                priority
+                />
+            </div>
+
+            <LoginCard />
+        
+           
         </div>
     )
 }
---
+++ client/src/components/login/LoginCard.tsx
@@ -0,0 +1,34 @@
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import "./index.css";
+const LoginCard = () => {
+    return (
+        <div className="relative w-[400px] mx-auto pt-[8%] ">
+
+
+            <Card className="bg-[var(--card-bg-color)] border-0 text-white">
+                <CardHeader>
+                    <CardTitle>Jump back in!</CardTitle>
+                    <CardDescription className="w-[70%]">Use your email or another service to continue with Canva (it’s free)!</CardDescription>
+                    {/* <CardAction>Card Action</CardAction> */}
+                </CardHeader>
+               <CardContent>
+                  
+                    <button className="w-full  py-2 rounded-[6px] text-sm  font-semibold mx-auto bg-[var(--card-bg-color)] cursor-pointer border-[1px] border-zinc-700 hover:bg-zinc-700 transition-all duration-300">Login with google</button>
+                </CardContent>
+                {/* <CardFooter>
+                    <p>Card Footer</p>
+                </CardFooter>  */}
+            </Card>
+        </div>
+    )
+}
+
+export default LoginCard;
